fd3365cf1d4d22766514dc28311abbaf946e2c28
Nockzb <132928494+Nockzb@users.noreply.github.com>
2024-01-11 22:03:18 +0100

    hasta aca ok

--- src/app/unidades-centro/unidades-centro.module.ts
+++ src/app/unidades-centro/unidades-centro.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CrudMaterialModule } from '../modules/crud-material/crud-material.module';
 
 import { UnidadesCentroRoutingModule } from './unidades-centro-routing.module';
-import { CrudMaterialModule } from '../modules/crud-material/crud-material.module';
+
 import { UnidadesCentrosComponent } from './unidades-centro.component';
 import { AddUnidadesCentroComponent } from './add-unidades-centro/add-unidades-centro.component';
 import { EditUnidadesCentroComponent } from './edit-unidades-centro/edit-unidades-centro.component';
