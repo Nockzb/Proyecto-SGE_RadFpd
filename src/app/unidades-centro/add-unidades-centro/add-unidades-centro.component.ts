afa0456c39482c78730ae1941580b4be349b788f
Nockzb <132928494+Nockzb@users.noreply.github.com>
2024-01-16 19:31:36 +0100

    ADD Y DELETE OK

--- src/app/unidades-centro/add-unidades-centro/add-unidades-centro.component.ts
+++ src/app/unidades-centro/add-unidades-centro/add-unidades-centro.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Unidad } from 'src/app/shared/interfaces/unidad';
-import { UnidadesService } from 'src/app/services/unidades.service';
 import { CLOSE, INVALID_FORM, ENTIDAD_UNIDAD } from 'src/app/shared/messages';
 import { Ciclo } from 'src/app/shared/interfaces/ciclo';
 import { CiclosService } from 'src/app/services/ciclos.service';
@@ -11,6 +9,8 @@ import { UnidadDual } from 'src/app/shared/interfaces/unidad-dual';
 import { UnidadesDualService } from 'src/app/services/unidades-dual.service';
 import { MotivoNodual } from 'src/app/shared/interfaces/motivo-nodual';
 import { MotivosNodualService } from 'src/app/services/motivos-nodual.service';
+import { UnidadesCentroService } from 'src/app/services/unidades-centro.service';
+import { UnidadCentro } from 'src/app/shared/interfaces/unidad-centro';
 
 @Component({
   selector: 'app-add-unidades-centro',
@@ -29,37 +29,28 @@ export class AddUnidadesCentroComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<AddUnidadesCentroComponent>,
     private snackBar: MatSnackBar,
-    private servicioUnidades: UnidadesService,
-    private servicioCiclos: CiclosService,
-    private servicioUnidadesDual: UnidadesDualService,
-    private servicioMotivosNodual: MotivosNodualService,
+    private servicioUnidadesCentro: UnidadesCentroService,
+    private servicioCiclos: CiclosService,    
     @Inject(MAT_DIALOG_DATA) public idEntidad: number,
 
   ){ }
 
   ngOnInit(): void {
-    this.unidadForm = new FormGroup({
-      id_entidad: new FormControl(this.idEntidad, Validators.required),
+    this.unidadForm = new FormGroup({      
       id_ciclo: new FormControl(null, Validators.required),
-      unidad: new FormControl(null, Validators.required),
-      plazas: new FormControl(null, Validators.required),
-      id_unidad_dual: new FormControl(null, Validators.required),
-      id_motivo_nodual: new FormControl(null),
+      unidad_centro: new FormControl(null, Validators.required),      
       observaciones: new FormControl(null)
     });
     this.ENTIDAD = ENTIDAD_UNIDAD;
 
-    this.getCiclos();
-    this.getUnidadesDual();
-    this.getMotivosNodual(this.idEntidad);
-    
+    this.getCiclos();    
   }
 
   async confirmAdd() {
     if (this.unidadForm.valid) {
-      const unidad = this.unidadForm.value as Unidad;
+      const unidadCentro = this.unidadForm.value as UnidadCentro;
 
-      const RESPONSE = await this.servicioUnidades.addUnidad(unidad).toPromise();
+      const RESPONSE = await this.servicioUnidadesCentro.addUnidadCentro(unidadCentro).toPromise();
       if (RESPONSE.ok) {
         this.snackBar.open(RESPONSE.message, CLOSE, { duration: 5000 });
         this.dialogRef.close({ok: RESPONSE.ok, data: RESPONSE.data});
@@ -78,26 +69,7 @@ export class AddUnidadesCentroComponent implements OnInit {
     }
   }
 
-  async getUnidadesDual(){
-    const RESPONSE = await this.servicioUnidadesDual.getAllUnidadesDual().toPromise();
-    if (RESPONSE.ok){
-      this.unidades_dual = RESPONSE.data as UnidadDual[];
-    }
-  }
-
-  async getMotivosNodual(idEntidad: number){
-    const RESPONSE = await this.servicioMotivosNodual.get(idEntidad).toPromise();
-    if (RESPONSE.ok){
-      this.motivos_nodual = RESPONSE.data as MotivoNodual[];
-    }
-  }
-
   onNoClick() {
     this.dialogRef.close({ok: false});
   }  
-
-  async unidadDual(ud: number) {
-    this.esDual = (ud > 1);
-    console.log(this.esDual);
-  }
 }
--- src/app/unidades-centro/delete-unidades-centro/delete-unidades-centro.component.ts
+++ src/app/unidades-centro/delete-unidades-centro/delete-unidades-centro.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UnidadesService } from 'src/app/services/unidades.service';
+import { CLOSE, ENTIDAD_CONTACTO, ENTIDAD_UNIDAD_CENTRO } from 'src/app/shared/messages';
+import { UnidadCentro } from 'src/app/shared/interfaces/unidad-centro';
+import { UnidadesCentroService } from 'src/app/services/unidades-centro.service';
+
 
 @Component({
   selector: 'app-delete-unidades-centro',
   templateUrl: './delete-unidades-centro.component.html',
   styleUrls: ['./delete-unidades-centro.component.scss']
 })
+
 export class DeleteUnidadesCentroComponent implements OnInit {
 
-  constructor() { }
+  ENTIDAD: String;
 
-  ngOnInit(): void {
+  constructor(
+    public dialogRef: MatDialogRef<DeleteUnidadesCentroComponent>,
+    @Inject(MAT_DIALOG_DATA) public unidad_centro: UnidadCentro,
+    public servicioUnidadesCentro: UnidadesCentroService,
+    public snackBar: MatSnackBar,
+  ) 
+  {   }
+
+  ngOnInit(): void {   
+    this.ENTIDAD = ENTIDAD_UNIDAD_CENTRO;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close({ ok: false });
+  }
+
+  async confirmDelete() {
+    const RESPONSE = await this.servicioUnidadesCentro.deleteUnidadCentro(this.unidad_centro.id_unidad_centro).toPromise();
+    if (RESPONSE.ok) {
+      this.snackBar.open(RESPONSE.message, CLOSE, { duration: 5000 });
+      this.dialogRef.close({ ok: RESPONSE.ok, data: RESPONSE.data });
+    } else { this.snackBar.open(RESPONSE.message, CLOSE, { duration: 5000 }); }
   }
 
 }
