daa35a574564f0bf5fc8a4f66a2c88fe48c295a0
Nockzb <132928494+Nockzb@users.noreply.github.com>
2024-01-17 18:42:17 +0100

    todo ok

--- src/app/unidades-centro/edit-unidades-centro/edit-unidades-centro.component.ts
+++ src/app/unidades-centro/edit-unidades-centro/edit-unidades-centro.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { UnidadesCentroService } from 'src/app/services/unidades-centro.service';
+import { Ciclo } from 'src/app/shared/interfaces/ciclo';
 import { UnidadCentro } from 'src/app/shared/interfaces/unidad-centro';
 import { CLOSE, ENTIDAD_UNIDAD_CENTRO, ERROR } from 'src/app/shared/messages';
-import { Ciclo } from 'src/app/shared/interfaces/ciclo';
 import { CiclosService } from 'src/app/services/ciclos.service';
-import { UnidadesCentroService } from 'src/app/services/unidades-centro.service';
 
 @Component({
   selector: 'app-edit-unidades-centro',
@@ -15,101 +15,42 @@ import { UnidadesCentroService } from 'src/app/services/unidades-centro.service'
 })
 export class EditUnidadesCentroComponent implements OnInit {
 
-  unidadCentroForm: FormGroup;
+  unidadesCentroForm: FormGroup;
   ciclos: Ciclo[];
   ENTIDAD: String;
-
 
   constructor(
     public dialogRef: MatDialogRef<EditUnidadesCentroComponent>,
     private snackBar: MatSnackBar,
     private servicioUnidadesCentro: UnidadesCentroService,
     private servicioCiclos: CiclosService,
-    @Inject(MAT_DIALOG_DATA) public unidadCentro: UnidadCentro
+    @Inject(MAT_DIALOG_DATA) public unidadesCentro: UnidadCentro
   ) { }
 
   ngOnInit(): void {
-    this.ENTIDAD = ENTIDAD_UNIDAD_CENTRO;
-    this.unidadCentroForm = new FormGroup({
-      id_unidad_centro: new FormControl(this.unidadCentro.id_unidad_centro),
-      unidad_centro: new FormControl(this.unidadCentro.unidad_centro, Validators.required),
-      id_ciclo:new FormControl(this.unidadCentro.id_ciclo, Validators.required),
-      observaciones: new FormControl(this.unidadCentro.observaciones)
-
+    this.ENTIDAD=ENTIDAD_UNIDAD_CENTRO;
+    this.unidadesCentroForm = new FormGroup({
+      id_unidad_centro: new FormControl(this.unidadesCentro.id_unidad_centro, Validators.required),
+      unidad_centro: new FormControl(this.unidadesCentro.unidad_centro, Validators.required),
+      id_ciclo: new FormControl(this.unidadesCentro.id_ciclo, Validators.required),
+      observaciones: new FormControl(this.unidadesCentro.observaciones)
     });
     this.getCiclos();
   }
 
-  // ngOnInit(): void {
-  //   this.setForm();
-  //   //this.setFilter();
-  // }
+  async confirmEdit(){
+    if (this.unidadesCentroForm.valid) {
+      const familiaForm = this.unidadesCentroForm.value;
 
-  // setForm() {
-  //   this.ENTIDAD = ENTIDAD_UNIDAD;
-  //   this.unidadCentroForm = new FormGroup({
-  //     id_unidad_centro: new FormControl(this.unidadCentro.id_unidad_centro, Validators.required),
-  //     unidad_centro: new FormControl(this.unidadCentro.unidad_centro, Validators.required),
-  //     id_ciclo: new FormControl(this.unidadCentro.id_ciclo, Validators.required),
-  //     observaciones: new FormControl(null)
-  //   });
-
-  //   this.getCiclos();
-  // }
-
-  // async confirmEdit(){
-  //   // console.log(this.unidadCentro);
-  //   if (this.unidadCentroForm.valid) {
-  //     const unidadCentroForm = this.unidadCentroForm.value;
-
-  //     const RESPONSE = await this.servicioUnidadesCentro.editUnidadCentro(unidadCentroForm).toPromise();
-  //     if (RESPONSE.ok) {
-  //       this.snackBar.open(RESPONSE.message, CLOSE, { duration: 5000 });
-  //       this.dialogRef.close({ ok: RESPONSE.ok, data: RESPONSE.data });
-  //     } else {
-  //       this.snackBar.open(RESPONSE.message, CLOSE, { duration: 5000 });
-  //     }
-  //   } else {
-  //     this.snackBar.open(ERROR, CLOSE, { duration: 5000 });
-  //   }
-  // }
-
-  // async confirmEdit(){
-  //   if (this.unidadCentroForm.valid) {
-  //     const unidadCentroForm = this.unidadCentroForm.value;
-
-  //     const RESPONSE = await this.servicioUnidadesCentro.editUnidadCentro(unidadCentroForm).toPromise();
-  //     if (RESPONSE.ok) {
-  //       this.snackBar.open(RESPONSE.message, CLOSE, { duration: 5000 });
-  //       this.dialogRef.close({ ok: RESPONSE.ok, data: RESPONSE.data });
-  //     } else { this.snackBar.open(ERROR, CLOSE, { duration: 5000 }); }
-  //   } else { this.snackBar.open(ERROR, CLOSE, { duration: 5000 }); }
-  // }
-  async confirmEdit() {
-    if (this.unidadCentroForm.valid) {
-      const unidadCentroForm = this.unidadCentroForm.value;
-
-      try {
-        const RESPONSE = await this.servicioUnidadesCentro.editUnidadCentro(unidadCentroForm).toPromise();
-
-        if (RESPONSE.ok) {
-          this.snackBar.open(RESPONSE.message || 'Operación exitosa', CLOSE, { duration: 5000 });
-          this.dialogRef.close({ ok: RESPONSE.ok, data: RESPONSE.data });
-        } else {
-          const errorMessage = RESPONSE.message || 'Ocurrió un fallo sin mensaje detallado';
-          console.error('Error al editar la unidad centro:', errorMessage);
-          this.snackBar.open(ERROR, CLOSE, { duration: 5000 });
-        }
-      } catch (error) {
-        console.error('Error inesperado:', error); // Agrega esta línea
-        this.snackBar.open(ERROR, CLOSE, { duration: 5000 });
-      }
-    } else {
-      this.snackBar.open(ERROR, CLOSE, { duration: 5000 });
-    }
+      const RESPONSE = await this.servicioUnidadesCentro.editUnidadCentro(familiaForm).toPromise();
+      if (RESPONSE.ok) {
+        this.snackBar.open(RESPONSE.message, CLOSE, { duration: 5000 });
+        this.dialogRef.close({ ok: RESPONSE.ok, data: RESPONSE.data });
+      } else { this.snackBar.open(ERROR, CLOSE, { duration: 5000 }); }
+    } else { this.snackBar.open(ERROR, CLOSE, { duration: 5000 }); }
   }
 
-  async getCiclos(){
+    async getCiclos(){
     const RESPONSE = await this.servicioCiclos.getAllCiclos().toPromise();
     if (RESPONSE.ok){
       this.ciclos = RESPONSE.data as Ciclo[];
@@ -119,4 +60,6 @@ export class EditUnidadesCentroComponent implements OnInit {
   onNoClick() {
     this.dialogRef.close({ ok: false });
   }
+
 }
+
